b7b81d7986f36b1fafe3d1de846502157e5cb4f5
hironaga <h-hironaga@ifreet.co.jp>
2020-07-26 01:11:18 +0900

    fix schema

--- migrations/schema/20200725161824_users.up.sql
+++ migrations/schema/20200725161824_users.up.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
     id bigserial PRIMARY KEY NOT NULL,
-    name VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR (255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
+    name varchar(255) UNIQUE NOT NULL,
+    password varchar(255) NOT NULL,
+    email varchar(255) UNIQUE NOT NULL,
+    created_at timestamptz NOT NULL,
+    updated_at timestamptz NOT NULL
 );
