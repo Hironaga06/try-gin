c18385e218f2bc82c60a809c788d2dd8ea52ef8e
hironaga <h-hironaga@ifreet.co.jp>
2020-07-25 17:11:23 +0900

    refactor script

--- script/create_migration_file.sh
+++ script/create_migration_file.sh
@@ -3,12 +3,14 @@
 set -e
 
 SCHEMA_PATH=$1
-TABLE_NAME=$2
+FILE_NAME=$2
 
 echo "===== create migration files ====="
-echo "$(date +%Y%m%d%H%M%S)_$TABLE_NAME.up.sql"
-touch $SCHEMA_PATH/$(date +%Y%m%d%H%M%S)_$TABLE_NAME.up.sql
-echo "$(date +%Y%m%d%H%M%S)_$TABLE_NAME.down.sql"
-touch $SCHEMA_PATH/$(date +%Y%m%d%H%M%S)_$TABLE_NAME.down.sql
+
+echo "$(date +%Y%m%d%H%M%S)_$FILE_NAME.up.sql"
+touch $SCHEMA_PATH/$(date +%Y%m%d%H%M%S)_$FILE_NAME.up.sql
+
+echo "$(date +%Y%m%d%H%M%S)_$FILE_NAME.down.sql"
+touch $SCHEMA_PATH/$(date +%Y%m%d%H%M%S)_$FILE_NAME.down.sql
 
 echo "finish"
