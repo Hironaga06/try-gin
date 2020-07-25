7c5f41afb1a921b35b707204c316da5caea73e0d
hironaga <h-hironaga@ifreet.co.jp>
2020-07-25 13:41:18 +0900

    modify main

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module try_gin
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.15
+	github.com/juju/errors v0.0.0-20200330140219-3fe23663418f
+	github.com/ngaut/log v0.0.0-20180314031856-b8e36e7ba5ac
+	github.com/pkg/errors v0.9.1
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
+)
--- main.go
+++ main.go
@@ -4,12 +4,30 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/ngaut/log"
+	"github.com/pkg/errors"
 )
 
 func main() {
+	db, err := initDB()
+	if err != nil {
+		log.Error(err)
+	}
+	defer db.Close()
+
 	router := gin.Default()
-	router.GET("/hello", func(c *gin.Context) {
+	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World")
 	})
-	router.Run(":8000")
+	router.Run(":8080")
+}
+
+func initDB() (*gorm.DB, error) {
+	db, err := gorm.Open("postgres", "host=postgres port=5432 user=postgres dbname=postgres password= sslmode=disable")
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return db, nil
 }
