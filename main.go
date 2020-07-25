73733caa0289bd26cd1305e2d126a40b5ff7c346
hironaga <h-hironaga@ifreet.co.jp>
2020-07-26 02:21:43 +0900

    refactor main.go

--- main.go
+++ main.go
@@ -6,27 +6,25 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/ngaut/log"
-	"github.com/pkg/errors"
+	"github.com/juju/errors"
+)
+
+var (
+	db *gorm.DB
 )
 
 func main() {
-	db, err := initDB()
-	if err != nil {
-		log.Error(err)
-	}
 	defer db.Close()
-
 	router := gin.Default()
 	router.GET("/", handler.Users(db))
 	router.POST("/users", handler.SetUser(db))
 	router.Run(":8080")
 }
 
-func initDB() (*gorm.DB, error) {
-	db, err := gorm.Open("postgres", "host=postgres port=5432 user=postgres dbname=postgres password=postgres sslmode=disable")
+func init() {
+	var err error
+	db, err = gorm.Open("postgres", "host=postgres port=5432 user=postgres dbname=postgres password=postgres sslmode=disable")
 	if err != nil {
-		return nil, errors.WithStack(err)
+		panic(errors.Trace(err))
 	}
-	return db, nil
 }
